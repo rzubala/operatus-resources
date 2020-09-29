5581c234dc3e468fba5e2806281dfb3d7a97ddbc
Rafał Zubala <rzubala@gmail.com>
2020-09-29 09:25:36 +0200

    resources

---
+++ order_page.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $(".order-panel-D").on("click", function (event) {
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+
+    var parentC = $(".order-panel-C").parent();
+    var parentD = $(".order-panel-D").parent();
+    var parentE = $(".order-panel-E").parent();
+    var firstCol = $(".first-col")
+    if (parentC.hasClass("d-none")) {
+      parentD.removeClass("col-lg-12");
+      parentD.addClass("col-lg-8");
+      firstCol.removeClass("col-lg-12")
+      firstCol.addClass("col-lg-9")
+    } else {
+      parentD.removeClass("col-lg-8");
+      parentD.addClass("col-lg-12");
+      firstCol.removeClass("col-lg-9")
+      firstCol.addClass("col-lg-12")
+    }
+    parentC.toggleClass("d-none");
+    parentE.toggleClass("d-none");
+  });
+});
