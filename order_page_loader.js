b0f3ca380944f65921ac4143f99e4bee2adce730
Rafał Zubala <rzubala@gmail.com>
2020-10-02 15:36:05 +0200

    expand side C

--- order_page_loader.js
+++ order_page_loader.js
@@ -30,9 +30,12 @@ function onDataRetrieved() {
 
 function handlePanelFullScreen() {
   $(".operatus-expand-full").prepend(getExpandFullScreen());
-
   $(".icon-expand").on("click", expandPanelFull);
   $(".icon-contract").on("click", expandPanelFull);
+
+  $(".order-panel-C > .inner-panel > .operatus-table-panel").append(getExpandSideScreen());
+  $(".icon-expand-side").on("click", expandPanelSide);
+  $(".icon-contract-side").on("click", expandPanelSide);
 }
 
 function handlePanelShowMore() {
@@ -75,6 +78,25 @@ function expandPanelFull(event) {
   }
   $(panel).find(".icon-expand").toggleClass("d-lg-block");
   $(panel).find(".icon-contract").toggleClass("d-none");
+}
+
+function expandPanelSide(event) {
+  const icon = event.target;
+  const columnPanel = icon.closest(".inner-panel");
+  const panel = icon.closest(".operatus-table-panel");
+  if (panel === undefined || columnPanel === undefined) {
+    return;
+  }
+  $(columnPanel).children().each(function () {
+      $(this).toggleClass("d-none");
+  });
+  $(panel).toggleClass("d-none") 
+  expandPanelC(event);
+
+  tableShowMore($(panel));
+
+  $(panel).find(".icon-expand-side").toggleClass("d-lg-block");
+  $(panel).find(".icon-contract-side").toggleClass("d-none");
 }
 
 function onShowMore(event) {
@@ -409,6 +431,19 @@ function getExpandFullScreen() {
   <div class="icon-contract d-none">
     <svg class="show-expand-svg" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrows-angle-contract" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
       <path fill-rule="evenodd" d="M.172 15.828a.5.5 0 0 0 .707 0l4.096-4.096V14.5a.5.5 0 1 0 1 0v-3.975a.5.5 0 0 0-.5-.5H1.5a.5.5 0 0 0 0 1h2.768L.172 15.121a.5.5 0 0 0 0 .707zM15.828.172a.5.5 0 0 0-.707 0l-4.096 4.096V1.5a.5.5 0 1 0-1 0v3.975a.5.5 0 0 0 .5.5H14.5a.5.5 0 0 0 0-1h-2.768L15.828.879a.5.5 0 0 0 0-.707z"></path>
+    </svg>
+  </div>`;
+}
+
+function getExpandSideScreen() {
+  return `<div class="icon-expand-side d-none d-lg-block">
+    <svg class="show-expand-side-svg" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-bar-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"></path>
+  </svg>
+  </div>
+    <div class="icon-contract-side d-none">
+      <svg class="show-expand-side-svg" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-bar-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z"></path>
     </svg>
   </div>`;
 }
