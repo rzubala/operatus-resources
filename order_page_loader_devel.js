87bbdf62dc7442aa319ca8690fc24cff9aa6ccec
Rafał Zubala <rzubala@gmail.com>
2020-10-20 15:58:31 +0200

    side expand icon

--- order_page_loader_devel.js
+++ order_page_loader_devel.js
@@ -41,7 +41,7 @@ function updateExhibitorColor() {
 }
 
 function handlePanelExpandSide() {
-  //$(".panel-col-title").prepend(getExpandSideScreen());
+  $(".panel-col-title").prepend(getExpandSideScreen());
 
   const panels = ($('.panel-col-title').closest('.order-panel-D'))
   for (const p of panels) {    
@@ -463,12 +463,12 @@ function getExpandFullScreen(classExpand, classContract) {
 
 function getExpandSideScreen() {
   return `<div class="icon-expand-side d-none d-lg-block">
-    <svg class="show-expand-side-svg" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-bar-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <svg class="show-expand-side-svg" width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-arrow-bar-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
       <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"></path>
   </svg>
   </div>
     <div class="icon-contract-side d-none">
-      <svg class="show-expand-side-svg" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-bar-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <svg class="show-expand-side-svg" width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-arrow-bar-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z"></path>
     </svg>
   </div>`;
