6b81bee71b9190a4a1eb6176947f513a3907f829
Rafał Zubala <rzubala@gmail.com>
2020-10-30 14:41:48 +0100

    expand/contract

--- order_page_loader_devel.js
+++ order_page_loader_devel.js
@@ -278,7 +278,7 @@ function reloadOpenStreetMaps() {
   }, 1000);
 
   $("#osm-place").parent().prepend(getFullMap());
-  $(".icon-full-map").on("click", showFullMap);
+  $(".icon-full-map").on("click", getFullCamera);
 }
 
 function handleCamera() {
@@ -759,6 +759,14 @@ function getFullMap() {
     <path fill-rule="evenodd" d="M15.817.113A.5.5 0 0 1 16 .5v14a.5.5 0 0 1-.402.49l-5 1a.502.502 0 0 1-.196 0L5.5 15.01l-4.902.98A.5.5 0 0 1 0 15.5v-14a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0L10.5.99l4.902-.98a.5.5 0 0 1 .415.103zM10 1.91l-4-.8v12.98l4 .8V1.91zm1 12.98l4-.8V1.11l-4 .8v12.98zm-6-.8V1.11l-4 .8v12.98l4-.8z"/>
   </svg>
   </div>`;
+}
+
+function getFullCamera() {
+  return `<div class="icon-full-camera">
+  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-aspect-ratio" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5v-9zM1.5 3a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"/>
+    <path fill-rule="evenodd" d="M2 4.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H3v2.5a.5.5 0 0 1-1 0v-3zm12 7a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H13V8.5a.5.5 0 0 1 1 0v3z"/>
+  </svg></div>`;
 }
 
 function getExpandTableRows() {
